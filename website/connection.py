dd77046c99d2df44150fe8fa019bfa47e651e0ea
astelmach01 <astelmach20@gmail.com>
2023-06-12 17:56:51 -0400

    update item

--- website/connection.py
+++ website/connection.py
@@ -9,21 +9,34 @@ client = boto3.client(
     region_name="us-east-2",
 )
 
+def get_update_params(body):
+    """Given a dictionary we generate an update expression and a dict of values
+    to update a dynamodb table.
+
+    Params:
+        body (dict): Parameters to use for formatting.
+
+    Returns:
+        update expression, dict of values.
+    """
+    update_expression = ["set "]
+    update_values = dict()
+
+    for key, val in body.items():
+        update_expression.append(f" {key} = :{key},")
+        update_values[f":{key}"] = val
+
+    return "".join(update_expression)[:-1], update_values
 
 def put_item(phone_number: str, **kwargs):
-    item = {"phone-number": {"S": phone_number}}
-
-    # Add the kwargs to the item dictionary
-    for key, value in kwargs.items():
-        item[key] = {"Value": {"S": str(value)}, "Action": "PUT"}
-
-    logging.info(f"Updating item {item}")
+    a, v = get_update_params(kwargs)
+    logging.info(f"Putting item for phone number {phone_number} with values {v} and expression {a}")
     response = client.update_item(
-        TableName=settings.TABLE_NAME,
-        Key={"phone-number": {"S": phone_number}},
-        AttributeUpdates=item
-    )
-    return response["ResponseMetadata"]["HTTPStatusCode"] == 200
+        Key={'phone-number': phone_number},
+        UpdateExpression=a,
+        ExpressionAttributeValues=dict(v)
+        )
+    return response
 
 
 def get_attribute(phone_number: str, attribute: str):
